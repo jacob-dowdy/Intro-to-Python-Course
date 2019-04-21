6f05e95a87c1eca805206e822e722b137d52e75e
Jacob Dowdy <jacobdowdy@rocketmail.com>
2019-04-21 15:00:15 -0400

    Create quick_calc.py

---
+++ quick_calc.py
@@ -0,0 +1,7 @@
+# a calculator that adds two user-provided numbers
+
+num1 = int(input("Give me a number: "))
+num2 = int(input("Give me another number: "))
+
+total = num1 + num2
+print("The sum of your numbers is ",total)
