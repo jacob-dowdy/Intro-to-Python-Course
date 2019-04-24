64172e6dbd822237ea90eae49539fb7ee4ba3afa
Jacob Dowdy <jacobdowdy@rocketmail.com>
2019-04-23 22:06:32 -0400

    Update 1 - make_doctor.py

--- 1 - make_doctor.py
+++ 1 - make_doctor.py
@@ -1,6 +1,6 @@
 # returns user's name with doctor in front of it
 def make_doctor(name):
-    return "Doctor " + name
+    return "Doctor " + name + " here"
 
 full_name = input("Please write your full name: ")
 print(make_doctor(full_name))
