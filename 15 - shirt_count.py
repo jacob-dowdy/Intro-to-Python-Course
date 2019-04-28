fd86d415be00253150bed75254a9d4fe7af30ee3
Jacob Dowdy <jacobdowdy@rocketmail.com>
2019-04-28 07:26:59 -0400

    Update 15 - shirt_count.py
    Prices added.

--- 15 - shirt_count.py
+++ 15 - shirt_count.py
@@ -1,11 +1,11 @@
-# program that takes the shirt order for a user
-# program can be exited by typing 'exit'
+# [ ] Create the Shirt Register program, run tests
 small_shirts = 0
 medium_shirts = 0
 large_shirts = 0
+total = 0
 
 while True:
-    size_entry = input("Please enter a size (S,M,L): ").capitalize()
+    size_entry = input("Please enter a size (S = $6, M = $7, L = $8). Enter 'exit' to calculate your total: ").capitalize()
     
     if size_entry.lower() == "exit":
         print("You've got",small_shirts,"smalls,",medium_shirts,"mediums, and",large_shirts,"larges.")
@@ -18,12 +18,5 @@ while True:
         large_shirts +=1
     else:
         print("Please enter a valid shirt type")
-        
-# test output
-'''Please enter a size (S,M,L): S
-Please enter a size (S,M,L): M
-Please enter a size (S,M,L): S
-Please enter a size (S,M,L): L
-Please enter a size (S,M,L): exit
-You've got 2 smalls, 1 mediums, and 1 larges. '''      
+   
        
